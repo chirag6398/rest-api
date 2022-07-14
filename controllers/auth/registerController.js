1d0c8932a895707b2abd6f4aeab7bd83df7fa88a
chirag6398 <agarwalchirag112@gmail.com>
2022-07-14 10:44:25 +0530

    make user model and jwt service sign method for token generation

--- controllers/auth/registerController.js
+++ controllers/auth/registerController.js
@@ -1,8 +1,58 @@
-
+import Joi from 'joi';
+import User from '../../models/user';
+import bcrypt from 'bcrypt';
+import JwtService from '../../services/JwtService';
+import CustomErrorHandler from '../../services/CustomErrorHandler';
 
 const registerController={
     async register(req,res,next){
+        const registerSchema = Joi.object({
+            name: Joi.string().min(3).max(30).required(),
+            email: Joi.string().email().required(),
+            password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+            repeat_password: Joi.ref('password')
+        });
+        const { error } = registerSchema.validate(req.body);
+        if (error) {
+            return next(error);
+        }
+    
+    try {
+        const exist = await User.exists({ email: req.body.email });
+        if (exist) {
+            return next(CustomErrorHandler.alreadyExist('This email is already taken.'));
+        }
+    } catch(err) {
+        return next(err);
+    }
+    const { name, email, password } = req.body;
 
+   
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    
+    const user = new User({
+        name,
+        email,
+        password: hashedPassword
+    });
+
+    let access_token;
+    
+    try {
+        const result = await user.save();
+        console.log(result);
+
+        
+        access_token = JwtService.sign({ _id: result._id, role: result.role });
+       
+        
+        
+    } catch(err) {
+        return next(err);
+    }
+
+        res.json({ access_token });
     }
 }
 
